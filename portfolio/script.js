4065665586b61d241a5ad84864327bfbaa8a10f6
JuliliOb <cheb.uliya@gmail.com>
2022-07-28 15:32:32 +0300

    add modal window

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,4 @@
-// Меню бургер
+//Burger menu
 
 const burger = document.querySelector('.burger');
 const navHeader = document.querySelector('.nav_header');
@@ -9,7 +9,7 @@ burger.addEventListener('click', function(e) {
     document.body.classList.toggle('_lock');
 });
 
-//Прокрутка при клике
+//Menu on click
 
 const navigationLink = document.querySelectorAll('.navigation_link[data-goto]');
 navigationLink.forEach(navigationLink => {
@@ -33,3 +33,41 @@ function onMenuLinkClick(e) {
     });
     e.preventDefault();
 }
+
+//Modal 
+const modalOpen = document.querySelectorAll('[data-modal]'),
+    modalClose = document.querySelector('[data-close]'),
+    modal = document.querySelector('.modal');
+
+modalOpen.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.classList.toggle('_lock');
+    });
+});
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.classList.toggle('_lock');
+}
+
+modalClose.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
+
+// Send request
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json));
